596144ac057fd51ee5d9b1d4e97dd3ae6432867a
suin <suinyeze@gmail.com>
2026-06-05 20:01:54 +0900

    TSKaigi 2026のスポンサー告知掲載を終了しました。
    ## 対象者
    
    - [x] 📖 読者
    - [ ] ✍️ 執筆者
    
    ## Problem
    
    TSKaigi 2026 は 2026年5月22日-23日に開催され、すでにイベントが終了していました。しかし、サイト上には引き続きスポンサー告知が表示されている状態でした。
    
    ## Solution
    
    TSKaigi 2026 のスポンサー告知掲載を終了しました。
    
    - docusaurus.config.js から announcement bar の告知を削除しました
    - src/theme/TOC/index.tsx から TOC 下部のバナー表示を削除しました
    - 使用箇所がなくなった static/img/sponsors/tskaigi2026-banner.png を削除しました
    
    ライセンス関連ファイルとバナー用スタイルは変更していません。
    
    ## Value
    
    読者が終了済みイベントの告知を見ずに、現在のサイト内容を確認できるようになります。
    
    ---
    
    Close #1127

--- src/theme/TOC/index.tsx
+++ src/theme/TOC/index.tsx
@@ -10,18 +10,6 @@ const LINK_ACTIVE_CLASS_NAME = "table-of-contents__link--active";
 export default function TOC({ className, ...props }: Props): JSX.Element {
   return (
     <div className={clsx(styles.tableOfContents, "thin-scrollbar", className)}>
-      <a
-        href="https://2026.tskaigi.org/"
-        target="_blank"
-        rel="noopener noreferrer"
-        className={styles.tocBanner}
-      >
-        <img
-          src="/img/sponsors/tskaigi2026-banner.png"
-          alt="TSKaigi 2026 — 2026年5月22日-23日 ベルサール羽田空港にて開催"
-          className={styles.tocBannerImage}
-        />
-      </a>
       <TOCItems
         {...props}
         linkClassName={LINK_CLASS_NAME}
